7fab5142821019a6d5b0127b9e7a9458815c8140
SolangeGalarza <kareng_5@hotmail.com>
2015-10-20 16:35:02 -0500

    Update index.php

--- ejercicio_clase/index.php
+++ ejercicio_clase/index.php
@@ -9,11 +9,42 @@ echo 'h1{
   text-align: center;
 }
 
+body{
+  background-color:#c8c8c8;
+  margin-left:30px;
+  margin-right:30px;
+}
+
+label{
+  font-weight:bold;
+
+}
+#nombre{
+  margin-left:30px;
+}
+#apellido{
+  margin-left:30px;
+}
+
+input{
+margin-bottom: 10px;
+}
+
+table{
+  margin:auto;
+}
+
 
 table, th, td{
   border: 1px solid #000000;
   border-collapse: collapse;
 }
+
+table th{
+  background-color: blue; 
+  color:white;
+}
+
 
 table tr:nth-child(even){
   background-color: #15efe8; 
@@ -57,10 +88,28 @@ while($i < 10){
   echo '</tr>';
 }
 
+
+
+
 echo '</tbody>';
-
-
-
 echo '</table>';
+
+
+echo '<div class:"label">';
+
+echo'<label for="nombre">Nombre</label>';
+echo'<input name="nombre" id="nombre" type="text" />';
+
+echo'<div>';
+
+
+echo'<label for="apellido">Apellido</label>';
+echo'<input name="apellido" id="apellido" type="text" />
+        </div>';
+
+echo' <button type="button" class="enviar" >Enviar</button>';
+
+
+
 echo '</body>';
 echo '</html>'; 
